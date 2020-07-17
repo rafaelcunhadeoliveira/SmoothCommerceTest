3193142fc1a27ff02c2929312ec52ad68bdc3a19
Rafael Cunha <rafael_ptc@hotmail.com>
2020-07-17 16:16:59 -0400

    Initial Commit

---
+++ SmoothCommerceTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmoothCommerceTest
+//
+//  Created by Rafael Cunha de Oliveira on 2020-07-17.
+//  Copyright © 2020 Rafael Cunha de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
